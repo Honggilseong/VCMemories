daa997200f595c83e081bf38664ba3ac59fa861a
unknown <hong.gilseong@gmail.com>
2022-06-08 13:42:25 +0900

    회원가입 페이지 라우터 셋업

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,7 +1,12 @@
-import { Routes as ReactRouterRoutes } from "react-router-dom";
+import { Route, Routes as ReactRouterRoutes } from "react-router-dom";
+import AuthPage from "./AuthPage";
 
 function Routes() {
-  return <ReactRouterRoutes></ReactRouterRoutes>;
+  return (
+    <ReactRouterRoutes>
+      <Route path="/auth" element={<AuthPage />} />
+    </ReactRouterRoutes>
+  );
 }
 
 export default Routes;
