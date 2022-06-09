3721f109acfa50091a888fe715fee5aa1974ff2d
unknown <hong.gilseong@gmail.com>
2022-06-10 00:03:59 +0900

    HomePage 헤더 컴포터는 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../component/HomePage/Header";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default HomePage;
