d801757a46100a41e1d2456809a4436b81def4cb
unknown <hong.gilseong@gmail.com>
2022-06-09 18:28:41 +0900

    AuthPageForm CSS 부분 => 컴포넌트로 작성

---
+++ src/component/AuthPage/AuthPageForm.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+interface UserInfo {
+  email: string;
+  password: string;
+  name: string;
+}
+
+interface Props {
+  handleSignUp: () => void;
+  handleShowPassword: () => void;
+  handleInputUserInfo: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: React.FormEvent<EventTarget>) => void;
+  userInfo: UserInfo;
+  showPassword: boolean;
+  isSignUp: boolean;
+}
+
+function AuthPageForm({
+  handleSignUp,
+  handleShowPassword,
+  handleInputUserInfo,
+  handleSubmit,
+  userInfo,
+  showPassword,
+  isSignUp,
+}: Props) {
+  return (
+    <div className="flex flex-row justify-center border-r-2 w-[550px]">
+      <div>
+        <h1 className="font-bold text-3xl mt-36 text-center">
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </h1>
+        <div className="border w-[300px] mt-5 mb-5 border-purple-500" />
+        <form onSubmit={handleSubmit}>
+          <span className="text-gray-500">Email</span>
+          <div className="h-11 mt-2 mb-5">
+            <input
+              name="email"
+              type="email"
+              className="w-full h-full border rounded-lg border-purple-500 focus: outline-purple-700 focus: outline-4 p-1"
+              placeholder="Enter your email"
+              onChange={handleInputUserInfo}
+              value={userInfo.email}
+            />
+          </div>
+          {isSignUp && (
+            <>
+              <span className="text-gray-500">Name</span>
+              <div className="h-11 mt-2 mb-5">
+                <input
+                  name="name"
+                  type="text"
+                  className="w-full h-full border rounded-lg border-purple-500 focus: outline-purple-700 focus: outline-4 p-1"
+                  placeholder="Enter your name"
+                  onChange={handleInputUserInfo}
+                  value={userInfo.name}
+                />
+              </div>
+            </>
+          )}
+          <span className="text-gray-500">Password</span>
+          <div className="h-11 mt-2 flex justify-center items-center border border-purple-500 focus-within:border-2 rounded-lg p-1">
+            <input
+              name="password"
+              type={showPassword ? "text" : "password"}
+              className="w-full h-full outline-none p-1 rounded-lg"
+              placeholder="Enter your password"
+              onChange={handleInputUserInfo}
+              value={userInfo.password}
+            />
+            <p onClick={handleShowPassword} className="cursor-pointer">
+              {showPassword ? "Hide" : "Show"}
+            </p>
+          </div>
+          <button
+            className="cursor-pointer bg-purple-500 flex items-center justify-center text-white h-11 mt-5 rounded-lg w-full"
+            type="submit"
+          >
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </button>
+        </form>
+        <p className="text-center mt-4 text-gray-500">
+          {isSignUp ? "You have an account? " : "You don't have an account? "}
+          <span
+            className="text-blue-500 font-bold cursor-pointer"
+            onClick={handleSignUp}
+          >
+            Click here
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPageForm;
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { createUser, signIn } from "../actions/authAction";
+import AuthPageForm from "../component/AuthPage/AuthPageForm";
 import { useAppDispatch } from "../reducers/store";
 import { useInternalRouter } from "./routing";
 
@@ -47,73 +48,19 @@ function AuthPage() {
     });
   };
 
+  const props = {
+    handleSignUp,
+    handleShowPassword,
+    handleInputUserInfo,
+    handleSubmit,
+    userInfo,
+    showPassword,
+    isSignUp,
+  };
+
   return (
     <div className="w-screen h-screen flex">
-      <div className="flex flex-row justify-center border-r-2 w-[550px]">
-        <div>
-          <h1 className="font-bold text-3xl mt-36 text-center">
-            {isSignUp ? "Sign Up" : "Sign In"}
-          </h1>
-          <div className="border w-[300px] mt-5 mb-5 border-purple-500" />
-          <form onSubmit={handleSubmit}>
-            <span className="text-gray-500">Email</span>
-            <div className="h-11 mt-2 mb-5">
-              <input
-                name="email"
-                type="email"
-                className="w-full h-full border rounded-lg border-purple-500 focus: outline-purple-700 focus: outline-4 p-1"
-                placeholder="Enter your email"
-                onChange={handleInputUserInfo}
-                value={userInfo.email}
-              />
-            </div>
-            {isSignUp && (
-              <>
-                <span className="text-gray-500">Name</span>
-                <div className="h-11 mt-2 mb-5">
-                  <input
-                    name="name"
-                    type="text"
-                    className="w-full h-full border rounded-lg border-purple-500 focus: outline-purple-700 focus: outline-4 p-1"
-                    placeholder="Enter your name"
-                    onChange={handleInputUserInfo}
-                    value={userInfo.name}
-                  />
-                </div>
-              </>
-            )}
-            <span className="text-gray-500">Password</span>
-            <div className="h-11 mt-2 flex justify-center items-center border border-purple-500 focus-within:border-2 rounded-lg p-1">
-              <input
-                name="password"
-                type={showPassword ? "text" : "password"}
-                className="w-full h-full outline-none p-1 rounded-lg"
-                placeholder="Enter your password"
-                onChange={handleInputUserInfo}
-                value={userInfo.password}
-              />
-              <p onClick={handleShowPassword} className="cursor-pointer">
-                {showPassword ? "Hide" : "Show"}
-              </p>
-            </div>
-            <button
-              className="cursor-pointer bg-purple-500 flex items-center justify-center text-white h-11 mt-5 rounded-lg w-full"
-              type="submit"
-            >
-              {isSignUp ? "Sign Up" : "Sign In"}
-            </button>
-          </form>
-          <p className="text-center mt-4 text-gray-500">
-            {isSignUp ? "You have an account? " : "You don't have an account? "}
-            <span
-              className="text-blue-500 font-bold cursor-pointer"
-              onClick={handleSignUp}
-            >
-              Click here
-            </span>
-          </p>
-        </div>
-      </div>
+      <AuthPageForm {...props} />
       <div className="flex-1">Background</div>
     </div>
   );
