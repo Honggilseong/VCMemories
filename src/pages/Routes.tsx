5201550762fef2421bf4748530fbe7125c23f18a
unknown <hong.gilseong@gmail.com>
2022-06-10 00:00:42 +0900

    HomePage 생성 후 라우터 작업

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes as ReactRouterRoutes } from "react-router-dom";
 import AuthPage from "./AuthPage";
+import HomePage from "./HomePage";
 
 function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/" element={<HomePage />} />
     </ReactRouterRoutes>
   );
 }
