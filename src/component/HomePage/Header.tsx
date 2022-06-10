a1b4e3ecf2bbae2ae3273f672a3bc5566286bf44
unknown <hong.gilseong@gmail.com>
2022-06-10 13:13:41 +0900

    헤더 기본 셋업 작업

--- src/component/HomePage/Header.tsx
+++ src/component/HomePage/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="w-full h-16 border-b-purple-800 border-b-2 bg-purple-500 text-white">
+      <div className="max-w-7xl flex justify-between h-full items-center mx-auto p-3 xl:p-0">
+        <h1 className="font-bold">VCMemories</h1>
+        <div>search</div>
+        <div>userInfo</div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
