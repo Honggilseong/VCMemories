18312f88cc7334f516d2b1106699290770421d8b
unknown <hong.gilseong@gmail.com>
2022-06-10 14:20:04 +0900

    서치바 CSS 작성

--- src/component/HomePage/Header.tsx
+++ src/component/HomePage/Header.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 function Header() {
+  const [searchValue, setSearchValue] = useState<string>("");
+  const handleSearchBar = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setSearchValue(event.target.value);
+  };
+
   return (
     <header className="w-full h-16 border-b-purple-800 border-b-2 bg-purple-500 text-white">
       <div className="max-w-7xl flex justify-between h-full items-center mx-auto p-3 xl:p-0">
         <h1 className="font-bold">VCMemories</h1>
-        <div>search</div>
+        <div>
+          <div className="flex justify-center items-center bg-white rounded-lg p-1 text text-black outline-1 outline outline-purple-900 focus-within:outline-[3px]">
+            <input
+              type="text"
+              className="rounded-lg outline-none"
+              placeholder="Search accounts"
+              onChange={handleSearchBar}
+              value={searchValue}
+            />
+            <BiSearchAlt2 color="black" size="20px" cursor="pointer" />
+          </div>
+        </div>
         <div>userInfo</div>
       </div>
     </header>
