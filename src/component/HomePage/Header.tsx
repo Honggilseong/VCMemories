0bf812a6c0af02f974dfd337c76609476ab76b06
unknown <hong.gilseong@gmail.com>
2022-06-10 18:13:43 +0900

    헤더 프로필 UserProfile 컴포넌트로 분리

--- src/component/HomePage/Header.tsx
+++ src/component/HomePage/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
+import UserProfile from "./Header/UserProfile";
 interface User {
   name: string;
   token: string;
@@ -38,24 +39,7 @@ function Header() {
             <BiSearchAlt2 color="black" size="20px" cursor="pointer" />
           </div>
         </div>
-        <div>
-          {user ? (
-            <div className="flex items-center justify-center cursor-pointer">
-              <h1 className="mr-4">{user.name}</h1>
-              <div className="rounded-full border-2 w-10 h-10 hidden lg:block">
-                <img
-                  src={user.profilePicture}
-                  alt="userProfilePicture"
-                  className="rounded-full"
-                />
-              </div>
-            </div>
-          ) : (
-            <div className="bg-purple-800 border-2 border-purple-900 p-2 rounded-lg cursor-pointer hover:bg-purple-700">
-              Sign In
-            </div>
-          )}
-        </div>
+        <UserProfile user={user} />
       </div>
     </header>
   );
---
+++ src/component/HomePage/Header/UserProfile.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+interface User {
+  name: string;
+  token: string;
+  email: string;
+  profilePicture: string;
+}
+interface Props {
+  user: User | null;
+}
+function UserProfile({ user }: Props) {
+  return (
+    <div>
+      {user ? (
+        <div className="flex items-center justify-center cursor-pointer">
+          <h1 className="mr-4">{user.name}</h1>
+          <div className="rounded-full border-2 w-10 h-10 hidden lg:block">
+            <img
+              src={user.profilePicture}
+              alt="userProfilePicture"
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="bg-purple-800 border-2 border-purple-900 p-2 rounded-lg cursor-pointer hover:bg-purple-700">
+          Sign In
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserProfile;
