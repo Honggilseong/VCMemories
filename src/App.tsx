86116067b74e84110f86692249c32192b03ee78d
unknown <hong.gilseong@gmail.com>
2022-06-12 10:58:02 +0900

    유저 게시글 작성 모달 app에 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Routes from "./pages/Routes";
+import Modal from "react-modal";
+import CreatePostModal from "./component/HomePage/Header/CreatePostModal";
 
+Modal.setAppElement("#root");
 function App() {
   return (
     <div>
       <Routes />
+      <CreatePostModal />
     </div>
   );
 }
