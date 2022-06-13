54bebb37db23ec4fff86b332abe254326fecfad3
unknown <hong.gilseong@gmail.com>
2022-06-13 13:34:48 +0900

    유저가 새로운 게시글을 작성할 수 있게 만들어 주는 모달 CSS 작성

--- src/component/HomePage/Header/CreatePostModal.tsx
+++ src/component/HomePage/Header/CreatePostModal.tsx
@@ -1,11 +1,143 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useSelector } from "react-redux";
+import { useInternalRouter } from "../../../pages/routing";
 import { RootState } from "../../../reducers";
+import { useAppDispatch } from "../../../reducers/store";
+import { closeModal } from "../../../actions/modalAction";
+interface NewPost {
+  title: string;
+  picture: string;
+  message: string;
+  tags: string;
+}
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 function CreatePostModal() {
+  const [newPost, setNewPost] = useState<NewPost>({
+    title: "",
+    picture: "",
+    message: "",
+    tags: "",
+  });
+  const navigate = useInternalRouter();
   const modal = useSelector((state: RootState) => state.modal);
-  return <Modal isOpen={modal.isModalOpen}>CreatePostModal</Modal>;
+  const dispatch = useAppDispatch();
+
+  const handleFile = (e: any) => {
+    const fileBlob = e.target.files[0];
+
+    setNewPost({ ...newPost, picture: URL.createObjectURL(fileBlob) });
+  };
+
+  const handleCloseModal = () => {
+    dispatch(closeModal());
+    setNewPost({
+      title: "",
+      picture: "",
+      message: "",
+      tags: "",
+    });
+  };
+
+  const handleInputNewUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewPost({ ...newPost, [name]: value });
+    console.log(newPost);
+  };
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("profile") || "");
+    if (!user) {
+      navigate.push("/");
+      return;
+    }
+    console.log(user);
+  }, [navigate]);
+
+  return (
+    <Modal
+      isOpen={modal.isModalOpen}
+      onRequestClose={handleCloseModal}
+      contentLabel="Example Modal"
+      style={customStyles}
+    >
+      <div
+        className={`w-96 h-96 ${!newPost.picture && "border border-gray-400"}`}
+      >
+        {newPost.picture && (
+          <img
+            src={newPost.picture}
+            alt="userPicture"
+            className="object-cover w-full h-full"
+          />
+        )}
+      </div>
+      <h2 className="mt-3 text-gray-500">Title</h2>
+      <div className="h-11 mt-2 flex justify-center items-center outline-1 outline outline-purple-500 focus-within:outline-2 rounded-lg p-1">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="w-full h-full rounded-lg outline-none"
+          value={newPost.title}
+          onChange={handleInputNewUser}
+        />
+      </div>
+      <h2 className="mt-3 text-gray-500">Message</h2>
+      <div className="h-11 mt-2 flex justify-center items-center outline-1 outline outline-purple-500 focus-within:outline-2 rounded-lg p-1">
+        <input
+          type="text"
+          name="message"
+          placeholder="Message"
+          className="w-full h-full rounded-lg outline-none"
+          value={newPost.message}
+          onChange={handleInputNewUser}
+        />
+      </div>
+      <h2 className="mt-3 text-gray-500">Tags(comma)</h2>
+      <div className="h-11 mt-2 flex justify-center items-center outline-1 outline outline-purple-500 focus-within:outline-2 rounded-lg p-1">
+        <input
+          type="text"
+          name="tags"
+          placeholder="Tags"
+          className="w-full h-full rounded-lg outline-none"
+          value={newPost.tags}
+          onChange={handleInputNewUser}
+        />
+      </div>
+      <div className="h-12 bg-purple-500 text-white mt-3 rounded-lg">
+        <label
+          htmlFor="pick"
+          className="w-full h-full flex justify-center items-center cursor-pointer"
+        >
+          Upload a image
+        </label>
+        <input
+          type="file"
+          id="pick"
+          className="hidden"
+          accept="image/png, image/jpeg , image/jpg"
+          onChange={handleFile}
+        />
+      </div>
+      <div
+        className="h-12 bg-gray-500 text-white mt-3 rounded-lg flex items-center justify-center cursor-pointer"
+        onClick={handleCloseModal}
+      >
+        <p>Close</p>
+      </div>
+    </Modal>
+  );
 }
 
 export default CreatePostModal;
