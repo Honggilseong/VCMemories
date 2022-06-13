9f91e8509fa1ef4c0a46318bdc6b0a641fdcddf6
unknown <hong.gilseong@gmail.com>
2022-06-13 13:33:15 +0900

    homepage에 body 컴포넌트 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Body from "../component/HomePage/Body";
 import Header from "../component/HomePage/Header";
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
