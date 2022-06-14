4965ee4f04793185641a536c5b89bdbe99d81a48
unknown <hong.gilseong@gmail.com>
2022-06-14 13:16:11 +0900

    newpost 페이로드 타입 추가

--- src/actions/postActionDispatch.ts
+++ src/actions/postActionDispatch.ts
@@ -1,7 +1,17 @@
 export const CREATE_POST = "CREATE_POST";
 
+export interface NewPost {
+  title: string;
+  picture: string;
+  message: string;
+  tags: string[];
+  name: string;
+  profilePicture: string;
+}
+
 export interface createPost {
   type: typeof CREATE_POST;
+  payload: NewPost;
 }
 
 export type postActionDispatch = createPost;
