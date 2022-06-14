af3f5edddc862c4f441d318fd2db8173f28047a2
unknown <hong.gilseong@gmail.com>
2022-06-14 13:18:23 +0900

    post 타입추가와 api 전송시 header config 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,12 +6,31 @@ interface FormData {
   password: string;
 }
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+interface PostData {
+  name: string;
+  picture: string;
+  title: string;
+  profilePicture: string;
+  tags: string[];
+  message: string;
+}
 
+const API = axios.create({ baseURL: "http://localhost:5000/" });
+const user = JSON.parse(localStorage.getItem("profile") || "");
+const config = {
+  headers: {
+    "Content-type": "application/json",
+    Authorization: "Bearer " + user.user.token,
+  },
+};
 export const signUp = (formData: FormData): AxiosPromise => {
   return API.post("/user/signup", formData);
 };
 
 export const signIn = (formData: FormData): AxiosPromise => {
   return API.post("/user/signin", formData);
+};
+
+export const createPost = (postData: PostData): AxiosPromise => {
+  return API.post("/posts/createpost", postData, config);
 };
