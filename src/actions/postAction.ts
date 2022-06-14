d81d958ade280788f875ede4417235906f4aa7bb
unknown <hong.gilseong@gmail.com>
2022-06-14 13:16:55 +0900

    액션 페이로드에 데이터 추가

--- src/actions/postAction.ts
+++ src/actions/postAction.ts
@@ -1,8 +1,11 @@
 import { Dispatch } from "redux";
-import { CREATE_POST } from "./postActionDispatch";
+import { CREATE_POST, NewPost } from "./postActionDispatch";
+import * as api from "../api";
 
-export const createPost = () => (dispatch: Dispatch) => {
+export const createPost = (newPost: NewPost) => async (dispatch: Dispatch) => {
+  const { data } = await api.createPost(newPost);
   dispatch({
     type: CREATE_POST,
+    payload: data,
   });
 };
