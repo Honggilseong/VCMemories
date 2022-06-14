23d2e9dfd3a9b92b41578848df0579b32844ab10
unknown <hong.gilseong@gmail.com>
2022-06-14 23:18:39 +0900

    홈페이지에서 getPosts 호출

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { useEffect } from "react";
+import { getPosts } from "../actions/postAction";
 import Body from "../component/HomePage/Body";
 import Header from "../component/HomePage/Header";
 
+import { useAppDispatch } from "../reducers/store";
+
 function HomePage() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
   return (
     <div>
       <Header />
