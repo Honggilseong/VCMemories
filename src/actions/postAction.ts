c43ddf22811a525faf92b44b1038539a6f530c79
unknown <hong.gilseong@gmail.com>
2022-06-16 14:15:59 +0900

    게시글 삭제를 위한 리덕스 액션 작성

--- src/actions/postAction.ts
+++ src/actions/postAction.ts
@@ -1,9 +1,15 @@
 import { Dispatch } from "redux";
-import { CREATE_POST, GET_POSTS, NewPost } from "./postActionDispatch";
+import {
+  CREATE_POST,
+  DELETE_POST,
+  GET_POSTS,
+  NewPost,
+} from "./postActionDispatch";
 import * as api from "../api";
 
 export const createPost = (newPost: NewPost) => async (dispatch: Dispatch) => {
   const { data } = await api.createPost(newPost);
+
   dispatch({
     type: CREATE_POST,
     payload: data,
@@ -17,3 +23,16 @@ export const getPosts = () => async (dispatch: Dispatch) => {
     payload: data,
   });
 };
+
+export const deletePost =
+  (id: string, userId: string) => async (dispatch: Dispatch) => {
+    try {
+      await api.deletePost(id, userId);
+      dispatch({
+        type: DELETE_POST,
+        payload: id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
--- src/actions/postActionDispatch.ts
+++ src/actions/postActionDispatch.ts
@@ -1,5 +1,6 @@
 export const CREATE_POST = "CREATE_POST";
 export const GET_POSTS = "GET_POSTS";
+export const DELETE_POST = "DELETE_POST";
 
 export interface NewPost {
   title: string;
@@ -8,6 +9,8 @@ export interface NewPost {
   tags: string[];
   name: string;
   profilePicture: string;
+  userId: string;
+  likes: string[];
 }
 
 export interface createPost {
@@ -22,4 +25,9 @@ export interface getPosts {
   payload: PostsState;
 }
 
-export type postActionDispatch = createPost | getPosts;
+export interface deletePost {
+  type: typeof DELETE_POST;
+  payload: string;
+}
+
+export type postActionDispatch = createPost | getPosts | deletePost;
