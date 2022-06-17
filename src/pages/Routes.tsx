411f76eaa760ac5138235f391cf0414f33a91402
unknown <hong.gilseong@gmail.com>
2022-06-17 18:24:40 +0900

    유저 프로필 페이지 작성

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes as ReactRouterRoutes } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import HomePage from "./HomePage";
+import ProfilePage from "./ProfilePage";
 
 function Routes() {
   return (
     <ReactRouterRoutes>
-      <Route path="/auth" element={<AuthPage />} />
       <Route path="/" element={<HomePage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </ReactRouterRoutes>
   );
 }
