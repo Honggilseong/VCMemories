39deea3d7602f289964480a0035f375ed8527f01
unknown <hong.gilseong@gmail.com>
2022-06-21 15:22:58 +0900

    게시글 좋아요 액션작성

--- src/actions/postAction.ts
+++ src/actions/postAction.ts
@@ -3,6 +3,7 @@ import {
   CREATE_POST,
   DELETE_POST,
   GET_POSTS,
+  LIKE_POST,
   NewPost,
 } from "./postActionDispatch";
 import * as api from "../api";
@@ -31,6 +32,19 @@ export const deletePost =
       dispatch({
         type: DELETE_POST,
         payload: id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+export const likePost =
+  (id: string, userId: string) => async (dispatch: Dispatch) => {
+    try {
+      const { data } = await api.likePost(id, userId);
+      dispatch({
+        type: LIKE_POST,
+        payload: data,
       });
     } catch (err) {
       console.log(err);
