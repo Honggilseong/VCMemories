417080db9d1c6cbd03918668ede9c6f629c4f51d
unknown <hong.gilseong@gmail.com>
2022-06-21 21:59:26 +0900

    컴바인 리듀서에 searchUser 연결

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,11 +3,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./authReducers";
 import modalReducer from "./modalReducers";
 import postReducer from "./postReducers";
+import SearchUserReducer from "./searchUserReducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   modal: modalReducer,
   post: postReducer,
+  searchUser: SearchUserReducer,
 });
 
 export default rootReducer;
