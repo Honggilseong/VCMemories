aab72d04469b717755adc9f4fe35b72f688b992d
unknown <hong.gilseong@gmail.com>
2022-06-22 22:27:15 +0900

    모든 유저 리덕스 연결

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,15 +1,17 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import allUsersReducer from "./allUsersReducers";
 
 import authReducer from "./authReducers";
 import modalReducer from "./modalReducers";
 import postReducer from "./postReducers";
-import SearchUserReducer from "./searchUserReducers";
+import searchUserReducer from "./searchUserReducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   modal: modalReducer,
   post: postReducer,
-  searchUser: SearchUserReducer,
+  searchUser: searchUserReducer,
+  allUsers: allUsersReducer,
 });
 
 export default rootReducer;
