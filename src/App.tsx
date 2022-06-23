2418292c1268b853195ed95c182ab1e935a46a9f
unknown <hong.gilseong@gmail.com>
2022-06-23 14:10:26 +0900

    검색 결과가 화면 아무곳이나 누르면 꺼지게 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import Routes from "./pages/Routes";
 import Modal from "react-modal";
 import CreatePostModal from "./component/HomePage/Header/CreatePostModal";
+import { useAppDispatch } from "./reducers/store";
+import { closeSearchResultsModal } from "./actions/modalAction";
+import { useSelector } from "react-redux";
+import { RootState } from "./reducers";
 
 Modal.setAppElement("#root");
 function App() {
+  const dispatch = useAppDispatch();
+  const modal = useSelector((state: RootState) => state.modal);
+  const handleCloseModals = () => {
+    if (modal.isSearchResultsModalOpen) dispatch(closeSearchResultsModal());
+  };
   return (
-    <div>
+    <div onClick={handleCloseModals}>
       <Routes />
       <CreatePostModal />
     </div>
