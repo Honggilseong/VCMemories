76b22918351064c73b16d9c6ccfc391b919c5cdf
unknown <hong.gilseong@gmail.com>
2022-07-02 14:15:37 +0900

    프로필에서 내 게시글 누르면 디테일 모달페이지 작성

---
+++ src/component/ProfilePage/ProfileInfoModal.tsx
@@ -0,0 +1,115 @@
+import { Image } from "cloudinary-react";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { AiFillHeart, AiOutlineComment, AiOutlineHeart } from "react-icons/ai";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Modal from "react-modal";
+import { likePost } from "../../actions/postAction";
+import { useAppDispatch } from "../../reducers/store";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+function ProfileInfoModal({
+  post,
+  isModalOpen,
+  setIsModalOpen,
+  isPostInfoOpen,
+  handleClickPostInfo,
+  handleDeletePost,
+}: any) {
+  const [likedPost, setLikedPost] = useState<boolean>(false);
+  const getUser = JSON.parse(localStorage.getItem("profile") || "");
+  const dispatch = useAppDispatch();
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  const handleLikePost = () => {
+    console.log("liked");
+    return;
+    dispatch(likePost(post._id, getUser.user._id));
+  };
+  console.log(post.likes.length);
+  useEffect(() => {
+    const likedPost = post.likes.findIndex(
+      (id: string) => id === getUser.user._id
+    );
+    if (likedPost === -1) {
+      setLikedPost(false);
+    } else {
+      setLikedPost(true);
+    }
+  }, [post.likes]);
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onRequestClose={handleCloseModal}
+      contentLabel="Example Modal"
+      style={customStyles}
+    >
+      <div className="h-14 border flex justify-between items-center px-3">
+        <div />
+        <h1>{post.name}</h1>
+
+        <div
+          className="cursor-pointer hover:bg-slate-300 rounded-full h-7 w-7 flex justify-center items-center relative"
+          onClick={handleClickPostInfo}
+        >
+          <BsThreeDotsVertical size={20} />
+          {isPostInfoOpen && (
+            <div className="absolute -left-2 border bg-white p-2 -bottom-10">
+              {post.userId === getUser.user._id && (
+                <p className="text-red-600" onClick={handleDeletePost}>
+                  Delete
+                </p>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="h-96 w-96 mx-auto">
+        <Image
+          key={post.picture}
+          cloudName={process.env.REACT_APP_CLOUDINARY_USERNAME}
+          publicId={post.picture}
+          className="w-full h-full"
+          crop="scale"
+        />
+      </div>
+      <div className="my-2">
+        <p>{post.message}</p>
+        <p>{post.tags.map((tag: string) => tag)}</p>
+        <p>{moment(post.createdAt).fromNow()}</p>
+      </div>
+      <div className="flex">
+        <div
+          className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
+          onClick={handleLikePost}
+        >
+          {likedPost ? (
+            <AiFillHeart size={30} color="red" />
+          ) : (
+            <AiOutlineHeart size={30} />
+          )}
+
+          <span>{post.likes.length}</span>
+        </div>
+        <div
+          className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
+          // onClick={handleClickComments}
+        >
+          <AiOutlineComment size={30} />
+          {/* <span>{post.comments.length}</span> */}
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ProfileInfoModal;
