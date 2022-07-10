eea919bef5f0303a4ed5f8cf7143c5f0ec9b1c49
unknown <hong.gilseong@gmail.com>
2022-07-10 22:49:52 +0900

    유저 notifications 알림 css 추가

--- src/component/HomePage/Header.tsx
+++ src/component/HomePage/Header.tsx
@@ -65,10 +65,12 @@ function Header() {
             className="cursor-pointer mr-4"
             onClick={handleCreatePost}
           />
-          <MdOutlineNotificationsNone
-            size={30}
-            className="cursor-pointer mr-4"
-          />
+          <div className="relative">
+            <MdOutlineNotificationsNone size={30} className="cursor-pointer" />
+            <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+              99
+            </span>
+          </div>
           <UserProfile user={user} />
         </div>
       </div>
