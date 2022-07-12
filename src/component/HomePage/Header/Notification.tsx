8d94da1038165ea1b6699253532f8e7e88fa4f34
unknown <hong.gilseong@gmail.com>
2022-07-12 15:53:42 +0900

    length 가 거짓이면 0이 출력이 되어서 안 나오게 변경

--- src/component/HomePage/Header/Notification.tsx
+++ src/component/HomePage/Header/Notification.tsx
@@ -7,7 +7,6 @@ function Notification() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const user = useSelector((state: RootState) => state.auth);
   const handleStateNotification = () => [setIsOpen((prev) => !prev)];
-  console.log(user);
   return (
     <div className="relative">
       <MdOutlineNotificationsNone
@@ -15,11 +14,11 @@ function Notification() {
         className="cursor-pointer"
         onClick={handleStateNotification}
       />
-      {user.notifications && (
+      {user.notifications?.length ? (
         <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
           {user.notifications.length}
         </span>
-      )}
+      ) : null}
       {isOpen && (
         <div className="absolute -bottom-10 -left-10 bg-white w-10 h-10 border-purple-500 border"></div>
       )}
