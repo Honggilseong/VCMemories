ec869f816c680bc0e60434a91d088aa89ff167b4
unknown <hong.gilseong@gmail.com>
2022-07-12 21:03:06 +0900

    알림 버튼 누르면 정보 보이게 작성

--- src/component/HomePage/Header/Notification.tsx
+++ src/component/HomePage/Header/Notification.tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { MdOutlineNotificationsNone } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../reducers";
-
+export interface Notifications {
+  _id: string;
+  read: boolean;
+  sender: string;
+  notificationType: string;
+}
 function Notification() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const user = useSelector((state: RootState) => state.auth);
@@ -19,9 +24,27 @@ function Notification() {
           {user.notifications.length}
         </span>
       ) : null}
-      {isOpen && (
-        <div className="absolute -bottom-10 -left-10 bg-white w-10 h-10 border-purple-500 border"></div>
-      )}
+      {isOpen ? (
+        user.notifications?.length ? (
+          <div className="absolute -bottom-50 -left-10 bg-white text-black border-purple-500 border z-50">
+            {user.notifications?.map((data: any) => (
+              <div
+                key={data._id}
+                className={`flex items-center p-3 border-2 cursor-pointer ${
+                  data.read && "bg-gray-400"
+                }`}
+              >
+                <p className="font-bold mr-2">{data.sender} </p>
+                <p className="mr-2">{data.notificationType}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="absolute -bottom-50 -left-10 bg-white text-black p-3 border-purple-500 border z-50">
+            <p>there's no notifications</p>
+          </div>
+        )
+      ) : null}
     </div>
   );
 }
