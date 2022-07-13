424222e712ccba361342c20da4cc1e450d90454b
unknown <hong.gilseong@gmail.com>
2022-07-13 16:49:39 +0900

    유저가 알림 아이콘을 눌렀을 때 api 통신 가능하게 작성

--- src/component/HomePage/Header/Notification.tsx
+++ src/component/HomePage/Header/Notification.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import { MdOutlineNotificationsNone } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../reducers";
+import { readNotifications } from "../../../actions/authAction";
+import { useAppDispatch } from "../../../reducers/store";
 export interface Notifications {
   _id: string;
   read: boolean;
@@ -10,11 +12,19 @@ export interface Notifications {
 }
 function Notification() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [notificationsCount, setNotificationsCount] = useState<number>(0);
   const user = useSelector((state: RootState) => state.auth);
+  const getUser = JSON.parse(localStorage.getItem("profile") || "");
   const dropDownRef = useRef(
     null
   ) as unknown as React.MutableRefObject<HTMLDivElement>;
-  const handleStateNotification = () => [setIsOpen((prev) => !prev)];
+  const dispatch = useAppDispatch();
+
+  const handleStateNotification = () => {
+    if (notificationsCount) dispatch(readNotifications(getUser.user._id));
+    setIsOpen((prev) => !prev);
+  };
+
   useEffect(() => {
     const handleClickPostInfo = (e: any) => {
       if (dropDownRef.current && !dropDownRef.current.contains(e.target))
@@ -24,6 +34,13 @@ function Notification() {
     return () =>
       document.removeEventListener("mousedown", handleClickPostInfo, false);
   }, []);
+
+  useEffect(() => {
+    const count = user.notifications?.filter(
+      (data: any) => data.read === false
+    ).length;
+    setNotificationsCount(count || 0);
+  }, [user]);
   return (
     <div className="relative">
       <MdOutlineNotificationsNone
@@ -31,9 +48,9 @@ function Notification() {
         className="cursor-pointer"
         onClick={handleStateNotification}
       />
-      {user.notifications?.length ? (
+      {notificationsCount ? (
         <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-          {user.notifications.length}
+          {notificationsCount}
         </span>
       ) : null}
       {isOpen ? (
