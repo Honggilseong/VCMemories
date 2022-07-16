c3d5628b313f9731f68e70b107c03b6936e5cb7e
unknown <hong.gilseong@gmail.com>
2022-07-16 20:28:01 +0900

    유저의 프로필에서 댓글을 확인할 수 있게 작성

--- src/component/ProfilePage/ProfileInfoModal.tsx
+++ src/component/ProfilePage/ProfileInfoModal.tsx
@@ -5,6 +5,7 @@ import { AiFillHeart, AiOutlineComment, AiOutlineHeart } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import Modal from "react-modal";
 import { likePost } from "../../actions/postAction";
+import { Comment } from "../../actions/postActionDispatch";
 import { useAppDispatch } from "../../reducers/store";
 const customStyles = {
   content: {
@@ -35,7 +36,6 @@ function ProfileInfoModal({
     return;
     dispatch(likePost(post._id, getUser.user._id));
   };
-  console.log(post.likes.length);
   useEffect(() => {
     const likedPost = post.likes.findIndex(
       (id: string) => id === getUser.user._id
@@ -53,59 +53,75 @@ function ProfileInfoModal({
       contentLabel="Example Modal"
       style={customStyles}
     >
-      <div className="h-14 border flex justify-between items-center px-3">
-        <div />
-        <h1>{post.name}</h1>
+      <div className="max-h-[550px]">
+        <div className="h-14 border flex justify-between items-center px-3">
+          <div />
+          <h1>{post.name}</h1>
 
-        <div
-          className="cursor-pointer hover:bg-slate-300 rounded-full h-7 w-7 flex justify-center items-center relative"
-          onClick={handleClickPostInfo}
-        >
-          <BsThreeDotsVertical size={20} />
-          {isPostInfoOpen && (
-            <div className="absolute -left-2 border bg-white p-2 -bottom-10">
-              {post.userId === getUser.user._id && (
-                <p className="text-red-600" onClick={handleDeletePost}>
-                  Delete
-                </p>
-              )}
+          <div
+            className="cursor-pointer hover:bg-slate-300 rounded-full h-7 w-7 flex justify-center items-center relative"
+            onClick={handleClickPostInfo}
+          >
+            <BsThreeDotsVertical size={20} />
+            {isPostInfoOpen && (
+              <div className="absolute -left-2 border bg-white p-2 -bottom-10">
+                {post.userId === getUser.user._id && (
+                  <p className="text-red-600" onClick={handleDeletePost}>
+                    Delete
+                  </p>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="h-96 w-96 mx-auto">
+          <Image
+            key={post.picture}
+            cloudName={process.env.REACT_APP_CLOUDINARY_USERNAME}
+            publicId={post.picture}
+            className="w-full h-full"
+            crop="scale"
+          />
+        </div>
+        <div className="my-2">
+          <p>{post.message}</p>
+          <p>{post.tags.map((tag: string) => tag)}</p>
+          <p>{moment(post.createdAt).fromNow()}</p>
+        </div>
+        <div className="flex">
+          <div
+            className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
+            onClick={handleLikePost}
+          >
+            {likedPost ? (
+              <AiFillHeart size={30} color="red" />
+            ) : (
+              <AiOutlineHeart size={30} />
+            )}
+
+            <span>{post.likes.length}</span>
+          </div>
+          <div
+            className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
+            // onClick={handleClickComments}
+          >
+            <AiOutlineComment size={30} />
+            <span>{post.comments.length}</span>
+          </div>
+        </div>
+        <div className="flex">
+          {post.comments.length ? (
+            post.comments.map((comment: Comment) => (
+              <div className="flex my-3">
+                <p className="font-bold mr-2">{comment.commentUserName}:</p>
+                <p>{comment.comment}</p>
+              </div>
+            ))
+          ) : (
+            <div className="h-20 flex justify-center items-center">
+              <p>There's no comments</p>
             </div>
           )}
-        </div>
-      </div>
-      <div className="h-96 w-96 mx-auto">
-        <Image
-          key={post.picture}
-          cloudName={process.env.REACT_APP_CLOUDINARY_USERNAME}
-          publicId={post.picture}
-          className="w-full h-full"
-          crop="scale"
-        />
-      </div>
-      <div className="my-2">
-        <p>{post.message}</p>
-        <p>{post.tags.map((tag: string) => tag)}</p>
-        <p>{moment(post.createdAt).fromNow()}</p>
-      </div>
-      <div className="flex">
-        <div
-          className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
-          onClick={handleLikePost}
-        >
-          {likedPost ? (
-            <AiFillHeart size={30} color="red" />
-          ) : (
-            <AiOutlineHeart size={30} />
-          )}
-
-          <span>{post.likes.length}</span>
-        </div>
-        <div
-          className="flex-[0.5] justify-center flex items-center h-14 border cursor-pointer"
-          // onClick={handleClickComments}
-        >
-          <AiOutlineComment size={30} />
-          {/* <span>{post.comments.length}</span> */}
         </div>
       </div>
     </Modal>
