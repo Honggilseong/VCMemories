bfb296ab1967e3aa350746d3e667250cb88983f7
unknown <hong.gilseong@gmail.com>
2022-07-21 15:49:22 +0900

    토스트 메시지 사용할 수 있게 유틸에 작성

---
+++ src/util/toast.ts
@@ -0,0 +1,24 @@
+import { toast } from "react-toastify";
+
+export const toastSuccess = (text: string) => {
+  toast.success(text, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+export const toastError = (text: string) => {
+  toast.error(text, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
