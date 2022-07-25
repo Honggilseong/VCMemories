faf3d5862b650d496e11b7fcfd6f63aef71ab2bd
unknown <hong.gilseong@gmail.com>
2022-07-25 21:12:22 +0900

    유저 정보가 있어야 게시글을 가져올 수 있게 작성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import { getAllUsers } from "../actions/allUsersAction";
 import { getPosts } from "../actions/postAction";
 import Body from "../component/HomePage/Body";
@@ -9,15 +10,17 @@ import { useAppDispatch } from "../reducers/store";
 function HomePage() {
   const dispatch = useAppDispatch();
   const effectRef = useRef(false);
+  const authUser = useSelector((state: any) => state.auth);
   useEffect(() => {
+    if (!authUser.name) return;
     if (effectRef.current === false) {
-      dispatch(getPosts());
+      dispatch(getPosts(authUser.following));
       dispatch(getAllUsers());
     }
     return () => {
       effectRef.current = true;
     };
-  }, []);
+  }, [authUser]);
   return (
     <div>
       <Header />
