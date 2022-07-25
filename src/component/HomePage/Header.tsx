c841d5bb5913916e32a08c33d50b452cdf6473fe
unknown <hong.gilseong@gmail.com>
2022-07-25 21:12:37 +0900

    필요 없는 함수 변수들 삭제

--- src/component/HomePage/Header.tsx
+++ src/component/HomePage/Header.tsx
@@ -11,30 +11,13 @@ import { useInternalRouter } from "../../pages/routing";
 import Notification from "./Header/Notification";
 import { getUserInfo } from "../../actions/authAction";
 
-interface User {
-  name: string;
-  token: string;
-  email: string;
-  profilePicture: string;
-  _id: string;
-}
-
 function Header() {
-  const getUser = JSON.parse(
-    localStorage.getItem("profile") || '{"user": { "token": "" }}'
-  );
   const navigate = useInternalRouter();
-  if (!getUser.user.token) navigate.push("/auth");
-  const [user, setUser] = useState<User>({
-    name: "",
-    token: "",
-    email: "",
-    profilePicture: "",
-    _id: "",
-  });
   const [searchValue, setSearchValue] = useState<string>("");
   const dispatch = useAppDispatch();
-  const isAuthenticated = JSON.parse(localStorage.getItem("profile") || "");
+  const isAuthenticated = JSON.parse(
+    localStorage.getItem("profile") || '{"user": { "token": "" }}'
+  );
   const handleSearchBar = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     dispatch(openSearchResultsModal());
@@ -53,19 +36,9 @@ function Header() {
     navigate.push(`/user/search/${name}`);
   };
   useEffect(() => {
-    if (!isAuthenticated) return;
-    setUser(isAuthenticated.user);
-    window.addEventListener("storage", handleStorage, false);
-    return () => {
-      window.removeEventListener("storage", handleStorage, false);
-    };
+    if (isAuthenticated.user._id)
+      dispatch(getUserInfo(isAuthenticated.user._id));
   }, []);
-  const handleStorage = () => {
-    setUser(isAuthenticated.user);
-  };
-  useEffect(() => {
-    if (user._id) dispatch(getUserInfo(user._id));
-  }, [user]);
   return (
     <header className="w-full h-16 border-b-purple-800 border-b-2 bg-purple-500 text-white">
       <div className="max-w-7xl flex justify-between h-full items-center mx-auto p-3 xl:p-0">
