5907c36645e16ec280f41c107eb857894473d2e2
unknown <hong.gilseong@gmail.com>
2022-07-29 13:16:49 +0900

    api 마다 config 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -22,16 +22,7 @@ interface NotificationSender {
   image?: string;
 }
 const API = axios.create({ baseURL: "http://localhost:5000/" });
-const user = JSON.parse(
-  localStorage.getItem("profile") || '{"user": { "token": "" }}'
-);
 
-const config = {
-  headers: {
-    "Content-type": "application/json",
-    Authorization: "Bearer " + user.user.token,
-  },
-};
 //user
 export const signUp = (formData: FormData): AxiosPromise => {
   return API.post("/user/signup", formData);
@@ -45,14 +36,38 @@ export const sendNotification = (
   id: string,
   sender: NotificationSender
 ): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/user/${id}/notification`, sender, config);
 };
 
 export const readNotification = (id: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/user/${id}/readnotification`, {}, config);
 };
 
 export const deleteNotifications = (id: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/user/${id}/deletenotifications`, {}, config);
 };
 
@@ -60,36 +75,100 @@ export const uploadProfileImage = (
   id: string,
   uploadImage: string
 ): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch("/user/uploadprofileimage", { id, uploadImage }, config);
 };
 
 export const followUser = (id: string, userId: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/user/followuser/${id}`, { userId }, config);
 };
 
 export const getUserInfo = (id: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.get(`/user/getuserinfo/${id}`, config);
 };
 
 export const getSearchingUser = (
   username: string | undefined
 ): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.get(`/user/search/${username}`, config);
 };
 
 export const getAllUsers = (): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.get("/user/getallusers", config);
 };
 
 //posts
 export const createPost = (postData: PostData): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.post("/posts/createpost", postData, config);
 };
 export const getPosts = (followingUsers: string[]): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.post("/posts/", followingUsers, config);
 };
 
 export const deletePost = (id: string, userId: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.delete(`/posts/${id}`, {
     headers: {
       "Content-type": "application/json",
@@ -100,6 +179,14 @@ export const deletePost = (id: string, userId: string): AxiosPromise => {
 };
 
 export const likePost = (id: string, userId: string): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/posts/${id}/likepost`, { userId }, config);
 };
 
@@ -110,11 +197,27 @@ interface Comment {
 }
 
 export const leaveComment = (id: string, comment: Comment): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.patch(`/posts/${id}/leavecomment`, { comment }, config);
 };
 
 //report
 
 export const reportPost = (post: any): AxiosPromise => {
+  const user = JSON.parse(localStorage.getItem("profile") || "");
+
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + user.user.token,
+    },
+  };
   return API.post("/report/", { post }, config);
 };
