f40632f34491c1bba28734aa711ea3d7c52ca5e1
unknown <hong.gilseong@gmail.com>
2022-08-04 15:26:07 +0900

    google analytics 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routes from "./pages/Routes";
 import Modal from "react-modal";
 import CreatePostModal from "./component/HomePage/Header/CreatePostModal";
+import ReactGA from "react-ga";
 import { useAppDispatch } from "./reducers/store";
 import { closeSearchResultsModal } from "./actions/modalAction";
 import { useSelector } from "react-redux";
@@ -9,12 +10,17 @@ import { RootState } from "./reducers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 Modal.setAppElement("#root");
+
 function App() {
   const dispatch = useAppDispatch();
   const modal = useSelector((state: RootState) => state.modal);
   const handleCloseModals = () => {
     if (modal.isSearchResultsModalOpen) dispatch(closeSearchResultsModal());
   };
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID || "");
+    ReactGA.pageview("Init page view");
+  }, []);
   return (
     <div onClick={handleCloseModals}>
       <Routes />
