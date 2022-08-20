f380d9bd810d7f5c5ac27ca1e95605950ae19b5e
unknown <hong.gilseong@gmail.com>
2022-08-20 17:10:46 +0900

    해시태그 게시물이 없을 때 없다는 문구가 나올 수 있게 작성

--- src/component/HashtagPage/Posts.tsx
+++ src/component/HashtagPage/Posts.tsx
@@ -5,10 +5,16 @@ interface Props {
 }
 function Posts({ hashtagPosts }: Props) {
   return (
-    <div className="max-w-[800px] mx-auto border p-3 xl:p-0">
-      {hashtagPosts.map((hashtagPost) => (
-        <Post key={hashtagPost._id} hashtagPost={hashtagPost} />
-      ))}
+    <div className="max-w-[800px] mx-auto p-3 xl:p-0">
+      {hashtagPosts.length > 0 ? (
+        hashtagPosts.map((hashtagPost) => (
+          <Post key={hashtagPost._id} hashtagPost={hashtagPost} />
+        ))
+      ) : (
+        <div className="w-full h-[200px] xl:h-[500px] flex justify-center items-center">
+          <p className="font-bold">No hashtag posts here yet...</p>
+        </div>
+      )}
     </div>
   );
 }
