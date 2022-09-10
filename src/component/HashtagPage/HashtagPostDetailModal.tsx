8ac3ad6b9362915a393bd862ee6eaa2178e8b078
unknown <hong.gilseong@gmail.com>
2022-09-10 14:37:46 +0900

    게시글 볼 때 스크롤 제거

--- src/component/HashtagPage/HashtagPostDetailModal.tsx
+++ src/component/HashtagPage/HashtagPostDetailModal.tsx
@@ -53,7 +53,7 @@ function HashtagPostDetailModal({
       contentLabel="Example Modal"
       style={customStyles}
     >
-      <div className="lg:h-[750px] w-full h-[500px] overflow-auto">
+      <div className="lg:h-[750px] w-full h-[500px] overflow-auto scrollbar-hide">
         <div className="h-14 border flex justify-between items-center px-3">
           <div />
           <h1 className="font-bold">{hashtagPost.name}</h1>
