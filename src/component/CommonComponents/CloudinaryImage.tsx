ea58f18864b41b708d1e73f47c25862345fd0516
unknown <hong.gilseong@gmail.com>
2022-09-11 20:01:37 +0900

    cloudinary 이미지 컴포넌트화

---
+++ src/component/CommonComponents/CloudinaryImage.tsx
@@ -0,0 +1,21 @@
+import { Image, Transformation } from "cloudinary-react";
+import React from "react";
+interface Props {
+  image: string;
+}
+function CloudinaryImage({ image }: Props) {
+  return (
+    <Image
+      key={image}
+      cloudName={process.env.REACT_APP_CLOUDINARY_USERNAME}
+      publicId={image}
+      className="w-full h-full"
+      crop="scale"
+      loading="lazy"
+    >
+      <Transformation quality="auto" />
+    </Image>
+  );
+}
+
+export default CloudinaryImage;
