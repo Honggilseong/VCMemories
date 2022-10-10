1442870f7a97aed415caee0c346d641a0469932a
unknown <hong.gilseong@gmail.com>
2022-10-10 17:30:33 +0900

    유저가 게시글을 수정할 수 있게 에딧페이지 작성

---
+++ src/pages/BoardPostEditPage.tsx
@@ -0,0 +1,11 @@
+import EditorComponent from "../component/CommonComponents/Forum/EditPage/EditorComponent";
+
+function BoardPostEditPage() {
+  return (
+    <div>
+      <EditorComponent isEdit />
+    </div>
+  );
+}
+
+export default BoardPostEditPage;
