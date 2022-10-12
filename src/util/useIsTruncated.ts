9ec858980c5db03dbb50a7822d8f5de7a595e2ca
unknown <hong.gilseong@gmail.com>
2022-10-12 21:45:18 +0900

    line clamp감지를 위한 훅작성

---
+++ src/util/useIsTruncated.ts
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { RefObject } from "react";
+export const useIsTruncated = (element: RefObject<HTMLParagraphElement>) => {
+  const determineIsTruncated = () => {
+    if (!element.current) return false;
+    return element.current.scrollHeight > element.current.clientHeight;
+  };
+  const [isTruncated, setIsTruncated] = useState(determineIsTruncated());
+  useEffect(() => {
+    const resizeListener = () => setIsTruncated(determineIsTruncated());
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, []);
+  return isTruncated;
+};
