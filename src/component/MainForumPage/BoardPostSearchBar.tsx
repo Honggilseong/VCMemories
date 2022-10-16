398ed0488f3fce8504b21e3bd246217c5111f63b
unknown <hong.gilseong@gmail.com>
2022-10-16 14:46:20 +0900

    게시글 검색 컴포넌트 인풋창 value 와 함수들 변경

--- src/component/MainForumPage/BoardPostSearchBar.tsx
+++ src/component/MainForumPage/BoardPostSearchBar.tsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+interface Props {
+  handleClickSearchBoardPost: () => void;
+  handleInputSearchValue: (e: any) => void;
+  searchValue: string;
+}
 
-function BoardPostSearchBar() {
-  const [value, setValue] = useState<string>("");
+function BoardPostSearchBar({
+  handleClickSearchBoardPost,
+  handleInputSearchValue,
+  searchValue,
+}: Props) {
   return (
     <div className="flex justify-center mt-5">
       <input
         type="text"
-        className="focus:outline-none p-1 border-2 rounded-lg focus:!border-purple-500"
+        className="focus:outline-none p-1 border-2 rounded-lg focus:border-purple-500"
         placeholder="Search"
-        value={value}
+        value={searchValue}
         name="search"
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleInputSearchValue}
       />
 
-      <button className="p-1 w-16 bg-purple-500 text-white rounded-lg ml-3">
+      <button
+        className="p-1 w-16 bg-purple-500 text-white rounded-lg ml-3"
+        onClick={handleClickSearchBoardPost}
+      >
         Search
       </button>
     </div>
